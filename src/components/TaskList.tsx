4804f16ab30e269e77ca81c5c107d7d988869455
Guilherme Cursino <guiiix93@gmail.com>
2021-07-20 22:33:35 -0300

    :tada: Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,16 +15,44 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if(!newTaskTitle){
+      return;
+    }
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]); // deste modo com o spread-operator "..." eu salvo um novo estado com os valores que tasks ja possuía mais o newTask.
+    setNewTaskTitle('');    
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const completedTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task);
+
+    setTasks(completedTask);
+
+    // Com o map eu percorro cada task para verificar qual a task tem o mesmo id que veio como parametro para encontrar a task desejada,
+    // com a task encontrada, através do spread-operator eu pego a task com o mesmo id do parametro, e nego com "!" sua propriedade isCompleted,
+    // assim eu mudo o que era false para true e marcando a checkbox, e clicando novamente transformo true para false desmarcando a checkbox.
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
-  }
+    
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
+
+    // desta forma ele filtra todo o array deixando os itens que nao tem o id que veio como parametro,
+    // automaticamente remove o item que foi passado o id no parametro da função.                                                          
+  }                                                              
 
   return (
     <section className="task-list container">
